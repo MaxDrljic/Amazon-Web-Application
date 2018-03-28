13185da494ca4f2f2195f5597d4f1b7898495bf4
MaxDrljic <max_spree@hotmail.com>
2018-03-28 08:53:11 +0200

    Aws setup, configs made to enable connection to bucket

---
+++ routes/seller.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const Product = require('../models/product');
+
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const s3 = new aws.S3({ accessKeyId: 'YourAccessKey', secretAccessKey: 'YourSecretAccessKey' });
+
+module.exports = router;
