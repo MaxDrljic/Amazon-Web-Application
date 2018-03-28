9a6739be95e30df7362c1650b7a6c6a50e8999a9
MaxDrljic <max_spree@hotmail.com>
2018-03-28 10:12:13 +0200

    Created upload function which will upload images to aws bucket

--- routes/seller.js
+++ routes/seller.js
@@ -4,6 +4,18 @@ const Product = require('../models/product');
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const s3 = new aws.S3({ accessKeyId: 'YourAccessKey', secretAccessKey: 'YourSecretAccessKey' });
+const s3 = new aws.S3({ accessKeyId: 'YourAccessKey', secretAccessKey: 'YourSecretKey' });
 
+const upload = multer({
+  storage:multerS3({
+    s3: s3,
+    bucket: 'amazoniumwebapplication',
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldName });
+    },
+    key: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldName });
+    },
+  })
+});
 module.exports = router;
