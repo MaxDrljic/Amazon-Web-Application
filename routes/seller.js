53b9cae83ef8acbc006713555bea3d7ea3603595
MaxDrljic <max_spree@hotmail.com>
2018-03-28 13:25:18 +0200

    Used faker to create dummy data to populate categories

--- routes/seller.js
+++ routes/seller.js
@@ -4,20 +4,22 @@ const Product = require('../models/product');
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const s3 = new aws.S3({ accessKeyId: 'YourAccessKey', secretAccessKey: 'YourSecretKey' });
+const s3 = new aws.S3({ accessKeyId: 'YourAccessKey', secretAccessKey: 'YourSecretAccessKey' });
+
+const faker = require('faker');
 
 const checkJWT = require('../middlewares/check-jwt');
 
-const upload = multer({
-  storage:multerS3({
+let upload = multer({
+  storage: multerS3({
     s3: s3,
     bucket: 'amazoniumwebapplication',
-    metadata: function(req, file, cb) {
-      cb(null, { fieldName: file.fieldName });
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
       cb(null, Date.now().toString())
-    },
+    }
   })
 });
 
@@ -50,6 +52,24 @@ router.route('/products')
     res.json({
       success: true,
       message: 'Successfully added the product!'
+    });
+  });
+
+  // Fake data for testing purposes
+  router.get('/faker/test', (req, res, next) => {
+    for (i = 0; i < 20; i++) {
+      let product = new Product();
+      product.category = '5aba87b61b469520cc14a996';
+      product.owner = '5ab4013c2e68291ee4418a15';
+      product.image = faker.image.cats();
+      product.title = faker.commerce.productName();
+      product.description = faker.lorem.words();
+      product.price = faker.commerce.price();
+      product.save();
+    }
+
+    res.json({
+      message: 'Successfully added 20 pictures'
     });
   });
 
