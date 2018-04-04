c0504e0419e66ca0d5fb935d4b67351f2d2aff51
MaxDrljic <max_spree@hotmail.com>
2018-04-04 09:10:04 +0200

    Added Stripe package & created order Schema

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const async = require('async');
+const stripe = require('stripe')('SecretKey');
 
 const Category = require('../models/category');
 const Product = require('../models/product');
